56ff69e93e65d37b536dcf6b633506d019cb7b95
Vesa Hagström <vesa.hagstrom@gmail.com>
2013-04-06 22:50:45 +0300

    game preloaderilla

--- src/game.js
+++ src/game.js
@@ -4,9 +4,14 @@
     // Initialize and start our game
     start: function() {
       Crafty.load([
-        ""
+        "assets/pie_sprite.png"
       ],
       function() {
+        
+        Crafty.sprite(66, "assets/pie_sprite.png", {
+           spr_player: [0,0]
+        });
+        
         // Start crafty and set a background color so that we can see it's working
         Crafty.init(800, 600);
         Crafty.background('#FFFFFF');
