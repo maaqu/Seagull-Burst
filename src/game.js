44f29dc1ea69c1de5002dc0738471881d5ffa93f
Sami Saada <sami.saada@gmail.com>
2013-04-06 22:49:47 +0300

    pie sprite pre-loading

--- src/game.js
+++ src/game.js
@@ -4,13 +4,13 @@
     // Initialize and start our game
     start: function() {
       Crafty.load([
-        ""
+        "assets/pie_sprite.png"
       ],
       function() {
         // Start crafty and set a background color so that we can see it's working
         Crafty.init(800, 600);
         Crafty.background('#FFFFFF');
-        
+
         // Simply start splashscreen
         Crafty.scene('Splashscreen');
       });
