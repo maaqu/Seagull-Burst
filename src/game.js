028a4278ba0da73fc9ea7192397f88beb3d6af21
Aurora <aurora@ubuntu.(none)>
2013-04-06 13:57:10 +0300

    real size

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ Game = {
 // Initialize and start our game
 start: function() {
 // Start crafty and set a background color so that we can see it's working
-Crafty.init(480, 320);
-Crafty.background('green');
+Crafty.init(800, 600);
+Crafty.background('#BFF9FF');
 }
 }
