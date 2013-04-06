2dcc3c479e7fcc47fd62bb19368c5ca57cef1075
Heikki Aitakangas <ornedan@gmail.com>
2013-04-06 22:25:25 +0300

    Resource pre-loading

--- src/game.js
+++ src/game.js
@@ -3,12 +3,17 @@
   var Game = {
     // Initialize and start our game
     start: function() {
-      // Start crafty and set a background color so that we can see it's working
-      Crafty.init(800, 600);
-      Crafty.background('#FFFFFF');
-      
-      // Simply start splashscreen
-      Crafty.scene('Splashscreen');
+      Crafty.load([
+        ""
+      ],
+      function() {
+        // Start crafty and set a background color so that we can see it's working
+        Crafty.init(800, 600);
+        Crafty.background('#FFFFFF');
+        
+        // Simply start splashscreen
+        Crafty.scene('Splashscreen');
+      });
     }
   }
 
