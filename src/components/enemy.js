d6aa88d5cfd9e965d3edad541fbcedffa47515df
Sami Saada <sami.saada@gmail.com>
2013-04-06 16:19:19 +0300

    enemy and powerup as components

---
+++ src/components/enemy.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+
+  // Generic powerup
+  Crafty.c('Enemy', {
+    init: function() {
+      this.requires("2D, Canvas, Color");
+    }
+  });
+}());
--- src/enemy.js
+++
@@ -1,8 +0,0 @@
-var Enemy = function(gfx) {
-  var _gfx = gfx;
-  return {
-    getGraphics: function() {
-      return _gfx;
-    }
-  }
-}
