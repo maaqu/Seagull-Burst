d1a921a70b3d9a48e3fbfe6c3c8819e4e4727bc2
maaqu <maa_q@hotmail.com>
2013-04-07 14:55:08 +0300

    kakkoslevelin pilvi oikealla tasolla

--- src/levels.js
+++ src/levels.js
@@ -102,7 +102,7 @@
     });
     level.addEntity(spawner);
 
-    var montySpawner = Crafty.e("Spawner").attr({x: 1000, y: 0}).setTime(200000)
+    var montySpawner = Crafty.e("Spawner").attr({x: 2000, y: 0}).setTime(200000)
       .bind("Spawn", function(attr) {
         level.addEntity(Crafty.e("Foot").attr({h: 500, x: attr.x, origY: -200, deltaY: 100}));
         level.addEntity(Crafty.e("Foot").attr({h: 500, x: attr.x+300, origY: -200, deltaY: 0, delta: -1}));
@@ -194,7 +194,7 @@
       .bind("Spawn", function(attr) {
         level.addEntity(Crafty.e("Foot").attr({h: 500, x: attr.x, origY: -200, deltaY: 100}));
         level.addEntity(Crafty.e("Foot").attr({h: 500, x: attr.x+300, origY: -200, deltaY: 0, delta: -1}));
-        level.addEntity(Crafty.e("Monty").attr({x: attr.x}));
+        level.addEntity(Crafty.e("Monty").attr({x: attr.x, y: -300}));
     });
     level.addEntity(montySpawner);
   };
